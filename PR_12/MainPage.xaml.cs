038342fcb1fe64937b51d806fce2feeb309feb08
civilmob1234 <raiden123926@gmail.com>
2022-05-03 23:40:17 +0300

    Добавьте файлы проекта.

---
+++ PR_12/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PR_12
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        int FibonacciNumber1 = 1;
+        int FibonacciNumber2 = 0;
+        int Result = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+       void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (Result <= 50)
+            {
+            FibonacciNumber2 += FibonacciNumber1;
+            Result = FibonacciNumber1;
+            label.Text = Result.ToString();
+            FibonacciNumber1 = FibonacciNumber2;
+            FibonacciNumber2 = Result;
+            label.Text = Result.ToString();
+            }
+        
+        }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
